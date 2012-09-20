ba3443827c26ae6ec04c5ee6aa027bcb10625ab7
windforze <windforze@hotmail.com>
2012-09-20 23:13:06 +0700

    Added files

---
+++ Site/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Site.App_Start {
+    using System.Web.Optimization;
+
+    public class BundleConfig {
+        public static void RegisterBundles(BundleCollection bundles) {
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                         "~/Scripts/jquery-{version}.js"));
+            
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                        .Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap/css")
+                        .Include("~/Content/bootstrap.css")
+                        .Include("~/Content/bootstrap-responsive.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+
+    }
+}
